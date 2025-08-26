e048276c27521f98a1be6aa3baef204c6bab95c6
Raznotovska Iuliia <hullabalooblues@gmail.com>
2025-08-27 00:41:15 +0300

    add smth

---
+++ lesson_5.py
@@ -0,0 +1,17 @@
+# user_age = int(input('Your age  '))
+#
+# true = True
+# false = False
+#
+# if user_age:
+#     print('*')
+#
+#
+# print(user_age)
+
+user_name = input('Enter name  ').title()
+
+if user_name == 'John':
+    print(f'Hello, {user_name}')
+else:
+    print('go away')
